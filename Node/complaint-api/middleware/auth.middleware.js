3d2ab3609ce6a8eaa77f6fb29a4498f44fae18df
pramodpandit72 <nwdpramod12305901@gmail.com>
2026-02-05 10:31:44 +0530

    Authorization added

--- Node/complaint-api/middleware/auth.middleware.js
+++ Node/complaint-api/middleware/auth.middleware.js
@@ -1,5 +1,12 @@
 const auth = (req, res, next) => {
   console.log("Auth checked");
+
+  const token = req.headers["authorization"];
+
+  if (!token || token !== "secret123") {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
   next();
 };
 
